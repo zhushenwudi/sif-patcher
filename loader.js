6c3bd958e4760b9b29e5ffc99d5c3db45bcb924b
zhushenwudi <55681140@163.com>
2024-12-31 11:20:24 +0800

    localization

--- loader.js
+++ loader.js
@@ -3,20 +3,43 @@ function update_status(status) {
     document.getElementById("status").innerText = status;
 }
 
-async function patch(file, android, newDomain, patches) {
+async function downloadFile(url) {
+    const response = await fetch(url);
+    const contentLength = response.headers.get('Content-Length');
+    const total = parseInt(contentLength, 10);
+    let loaded = 0;
+
+    const reader = response.body.getReader();
+    const chunks = [];
+
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        chunks.push(value);
+        loaded += value.length;
+
+        // 计算并显示下载进度
+        const progress = (loaded / total) * 100;
+        update_status(`下载中... ${progress.toFixed(2)}%`);
+    }
+
+    // 将所有块合并为一个 Blob
+    return new Blob(chunks);
+}
+
+async function patch(android, newDomain, patches) {
     if (newDomain.endsWith("/")) newDomain = newDomain.slice(0, -1);
 
-    if (!file) {
-        console.log("downloading");
-        update_status("Downloading...");
-        file = await (await fetch("https://ethanthesleepy.one/public/lovelive/sif/lovelive-community." + (android ? "apk" : "ipa"))).blob();
-        console.log("downloaded");
-    }
+    console.log("downloading");
+    update_status("下载中...");
+    let file = await downloadFile("https://file.zhushenwudi.top/pd/1/SIF_CLIENT/lovelive-community." + (android ? "apk" : "ipa"));
+    console.log("downloaded");
     console.log("loaded");
-    update_status("Opening file");
+    update_status("解压客户端...");
     const zip = new JSZip();
     await zip.loadAsync(file);
-    update_status("Getting current config");
+    update_status("获取当前配置...");
 
     let basePath = android ? "" : "Payload/LoveLive.app/";
     let server_file;
@@ -51,21 +74,21 @@ async function patch(file, android, newDomain, patches) {
 
     //console.log(server_file);
     FS.writeFile("/server_info.json", new Uint8Array(server_file));
-    update_status("Decrypting");
+    update_status("解密中...");
     Module.callMain(["server_info.json"]);
-    update_status("Patching domain");
+    update_status("修补地址中...");
     const data = FS.readFile("/server_info.json", {encoding: 'utf8'});
     const currentData = JSON.parse(data);
     const currentDomain = currentData.domain;
     const newData = data.split(currentDomain).join(newDomain);
     FS.writeFile("/server_info.json", newData);
-    update_status("Encrypting");
+    update_status("加密中...");
     Module.callMain(["-e", "-j", "server_info.json"]);
     const new_server_info = FS.readFile("/server_info.json");
 
     let type;
     let ext;
-    update_status("Applying changes");
+    update_status("打包中...");
     if (android) {
         let server_info = await zip.file("assets/AppAssets.zip").async("arraybuffer");
         const zip2 = new JSZip();
@@ -94,11 +117,11 @@ async function patch(file, android, newDomain, patches) {
         ext = "ipa";
     }
     let finalized = await zip.generateAsync({type: "uint8array"});
-    update_status("Finalizing");
+    update_status("请选择下载成果物");
     const downloadUrl = URL.createObjectURL(new Blob([finalized], {type: type}));
     const b = document.createElement("a");
     b.href = URL.createObjectURL(new Blob([new_server_info], {type: "application/json"}))
-    b.innerText = "Download patched server_info.json (you probably dont need this)";
+    b.innerText = "修补文件：server_info.json（你可能不需要这个）";
     b.download = "server_info.json";
     document.body.appendChild(b);
 
@@ -107,26 +130,26 @@ async function patch(file, android, newDomain, patches) {
 
     const a = document.createElement("a");
     a.href = downloadUrl;
-    a.innerText = "Download";
+    a.innerText = "下载客户端";
     a.download = "lovelive-patched."+ext;
     document.body.appendChild(a);
-    update_status("Done!");
+    update_status("完成！");
     if (android) {
         const p = document.createElement("p");
-        p.innerHTML = "Package is not signed. Using <a href=\"https://github.com/patrickfav/uber-apk-signer/releases\">uber-apk-signer</a>, sign it with the command `java -jar uber-apk-signer-<version>.jar -a lovelive.apk`.";
+        p.innerHTML = "修补的安装包未签名，可以使用 <a href=\"https://github.com/patrickfav/uber-apk-signer/releases\">uber-apk-signer</a>, 使用如下命令 `java -jar uber-apk-signer-<version>.jar -a lovelive.apk` 进行签名.";
         document.body.appendChild(p);
     }
 }
 
-function init_honoka(file, android, newDomain, patches) {
+function init_honoka(android, newDomain, patches) {
     window.Module = {
         noInitialRun: true,
         onRuntimeInitialized: async function() {
             try {
-                await patch(file, android, newDomain, patches);
+                await patch(android, newDomain, patches);
             } catch(e) {
                 console.warn(e);
-                document.getElementById("status").innerText = "It didnt work. Are your files/options invalid?";
+                document.getElementById("status").innerText = "失败。您的文件/选项无效吗？";
             }
         },
         arguments: [],
